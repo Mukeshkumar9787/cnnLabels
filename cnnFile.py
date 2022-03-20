ffd2c6888bd1c29f6645a75577a560a96931f70b
Mukeshkumar <mukeshkumar87086@gmail.com>
2022-03-20 23:24:03 +0530

    Update cnnFile.py

--- cnnFile.py
+++ cnnFile.py
@@ -169,5 +169,4 @@ print("accuracy_score:",accuracy_score(train_y, predictions))
 # loading library
 import pickle
 # create an iterator object with write permission - model.pkl
-with open('model_pickle.pkl', 'a+') as files:
-    pickle.dump(model, files)
+pickle.dump(model, "model.pkl")
